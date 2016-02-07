d6b6b8f8f098d27f9ffd34066c73b317f6937abc
YeWang0 <yewang0910@gmail.com>
2016-02-06 20:37:10 -0500

    algorithm

---
+++ LongestPalindrome.py
@@ -0,0 +1,18 @@
+def checkPalidroma(s):
+    for i in xrange(len(s)//2+1):
+        if s[i]!=s[len(s)-i-1]:
+            return False
+    return True
+
+def longestPalidroma(s):
+    max=len(s)
+    while max>=1:
+        for x in xrange(len(s)-max+1):
+            if checkPalidroma(s[x:x+max]):
+                return s[x:x+max]
+        max-=1
+    return max
+
+print longestPalidroma("kyyrjtdplseovzwjkykrjwhxquwxsfsorjiumvxjhjmgeueafubtonhlerrgsgohfosqssmizcuqryqomsipovhhodpfyudtusjhonlqabhxfahfcjqxyckycstcqwxvicwkjeuboerkmjshfgiglceycmycadpnvoeaurqatesivajoqdilynbcihnidbizwkuaoegmytopzdmvvoewvhebqzskseeubnretjgnmyjwwgcooytfojeuzcuyhsznbcaiqpwcyusyyywqmmvqzvvceylnuwcbxybhqpvjumzomnabrjgcfaabqmiotlfojnyuolostmtacbwmwlqdfkbfikusuqtupdwdrjwqmuudbcvtpieiwteqbeyfyqejglmxofdjksqmzeugwvuniaxdrunyunnqpbnfbgqemvamaxuhjbyzqmhalrprhnindrkbopwbwsjeqrmyqipnqvjqzpjalqyfvaavyhytetllzupxjwozdfpmjhjlrnitnjgapzrakcqahaqetwllaaiadalmxgvpawqpgecojxfvcgxsbrldktufdrogkogbltcezflyctklpqrjymqzyzmtlssnavzcquytcskcnjzzrytsvawkavzboncxlhqfiofuohehaygxidxsofhmhzygklliovnwqbwwiiyarxtoihvjkdrzqsnmhdtdlpckuayhtfyirnhkrhbrwkdymjrjklonyggqnxhfvtkqxoicakzsxmgczpwhpkzcntkcwhkdkxvfnjbvjjoumczjyvdgkfukfuldolqnauvoyhoheoqvpwoisniv")
+
+
