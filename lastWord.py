12bbc92265b2a0605e6dd3b60e1e578f214d7b3e
YeWang0 <yewang0910@gmail.com>
2016-02-08 20:47:13 -0500

    length of last word

---
+++ lastWord.py
@@ -0,0 +1,13 @@
+# length of last word
+def lastword(str):
+    s=0
+    count=0
+    for x in str[::-1]:
+        if x!=' ':
+            s=1
+        if s==1 and x!=' ':
+            count+=1
+        if s==1 and x==' ':
+            return count
+    return count
+print(lastword("hello word "))
