4d81630265caa67aad3991f4e9c0ab412695e301
YeWang0 <yewang0910@gmail.com>
2016-08-06 21:42:56 -0400

    subset of nums

---
+++ subset.py
@@ -0,0 +1,42 @@
+def subset(n):
+    list=[i for i in xrange(1,n+1)]
+    result=[]
+    def combine(n,k):
+        # pick n numbers from k
+        def pick(list,ln,pre):
+            if ln==0:
+                result.append(pre)
+                return
+            if list==[]:
+                return
+
+            for i in xrange(len(list)):
+                temp=pre[:]
+                temp.append(list[i])
+                pick(list[i+1:],ln-1,temp)
+        def Opick(list,ln,pre):
+            if ln==0:
+                result.append(pre)
+                return
+            if list==[]:
+                return
+
+            for i in xrange(len(list)):
+                temp=pre[:]
+                temp.remove(list[i])
+                Opick(list[i+1:],ln-1,temp)
+
+        # pick(list,k,[])
+
+        if 2*k>n:
+            k=n-k
+            Opick(list,k,list)
+        else:
+            pick(list,k,[])
+
+
+        print result
+    for i in xrange(n):
+        combine(n,i)
+subset(5)
+
