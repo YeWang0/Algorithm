ae867583f786d643659cbe9cf7eaec83877c2bf6
YeWang0 <yewang0910@gmail.com>
2016-09-08 19:04:11 -0400

    travel around gas stations

--- GasStation.py
+++ GasStation.py
@@ -1,26 +1,26 @@
 def canCircuit(gas,cost):
-    poss=[0 for i  in len(gas)]
+    poss=[0 for i  in xrange(len(gas))]
     result=[]
-    for i in len(gas):
+    for i in xrange(len(gas)):
         if gas[i]>=cost[i]:
             poss[i]=1
     def check(index):
         left=0
         for i in xrange(index,len(gas)):
             temp=gas[i]-cost[i]
-            if temp+left>0:
+            if temp+left>=0:
                 left=temp+left
             else:
                 return False
         for i in xrange(0,index):
             temp=gas[i]-cost[i]
-            if temp+left>0:
+            if temp+left>=0:
                 left=temp+left
             else:
                 return False
         return True
-    for i in len(gas):
-        if i==1:
+    for i in xrange(len(gas)):
+        if poss[i]==1:
             if check(i):
-                result.append(i)
-    return result
+                return i
+    return -1
